695f2217f1f7069534d5bbcad58f3aa0350b9ba6
Victor Figueroa <manifestus@pop-os.localdomain>
2023-04-19 16:01:21 -0600

    Staging App for test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { getEnvPath } from './env.helper';
+import { ApiModule } from './api/api.module';
+import { TypeOrmConfigService } from './typeorm/typeorm.service';
+
+const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ envFilePath, isGlobal: true }),
+    TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
+    ApiModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
